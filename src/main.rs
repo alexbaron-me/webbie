cd315c46238305d5b9091d34dfacfdf9079bcec7
Alexander Baron <alexander.baron1@rwth-aachen.de>
2025-07-29 17:26:27 +0200

    feat: POC syntax highlighting for request body

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use clap::Parser;
 use colored::Colorize;
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
 use webbie::{start_server, Request, RequestLogger};
 
 #[derive(Parser, Debug)]
@@ -26,7 +32,30 @@ impl RequestLogger for ConsoleLogger {
             .collect::<Vec<_>>()
             .join("\n");
 
-        let body_str = String::from_utf8_lossy(req.body().as_ref());
+        let body_str = if req.body().len() > 0 {
+            // TODO: Move this away from the hot path
+            let ss = SyntaxSet::load_defaults_newlines();
+            let ts = ThemeSet::load_defaults();
+
+            // TODO: Handle different content types, depending on MIME type
+            let syntax = ss.find_syntax_by_extension("json").unwrap();
+            // TODO: Better theme selection
+            let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+
+            let raw = String::from_utf8_lossy(req.body().as_ref());
+
+            // TODO: No need to allocate a new vector here, can use a stream instead
+            let mut lines = vec![];
+            for line in LinesWithEndings::from(raw.as_ref()) {
+                let ranges: Vec<(Style, &str)> = h.highlight_line(line, &ss).unwrap();
+                let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+                lines.push(escaped);
+            }
+
+            lines.join("\n")
+        } else {
+            "".into()
+        };
 
         let query = if req.query().is_empty() {
             String::new()
